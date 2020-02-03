77e05fd1b5bbde24f23afaf70ff07e991884d9b9
siko408 <gamerbloxhd@gmail.com>
2020-02-03 12:51:46 -0800

    [Progress] User Authenthication

--- buddiconnect/urls.py
+++ buddiconnect/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.Homepage.as_view(), name='homePage'),
+    path(r'^signup/$', views.signup, name='signup'),
 ]
--- buddiconnect/views.py
+++ buddiconnect/views.py
@@ -1,7 +1,10 @@
-# from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from django.views import View
 from django.template import loader
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 # Create your views here.
 
 
@@ -12,7 +15,19 @@ class Homepage(View):
         template = loader.get_template('home_Screen/home.html')  #  Templates folder needs to be within App is True
         return HttpResponse(template.render({}, request))
 
-    def post(self, request):
-        """ Sign Up must be  """
-        if request == 'POST':
-            pass
+
+def signup(request):
+    """ This will prompt User Creation and with form"""
+    if request.method == 'POST':
+        print("Request is post")
+        form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(request, user)
+        return redirect('homePage')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signupPage/signupPage.html', {'form': form})
